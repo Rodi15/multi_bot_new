db60f4a73c4d9b9c1e646b5a2d920af16182b5b8
pucka <validpucka@gmail.com>
2020-01-28 22:22:09 +0300

    update-message

--- events/message.js
+++ events/message.js
@@ -1,6 +1,8 @@
 const config = require('../config.json');
 const prefix = config.prefix;
 
+const moment = require('moment');
+
 module.exports = (client, message) => {
 	if(message.author.bot) return;
 
@@ -13,4 +15,19 @@ module.exports = (client, message) => {
 		if(!command) return;
 		command.run(client, message, args);
 	}
+
+    	var msgCon = message.content.toLowerCase();
+	
+    	if(msgCon === 'привет' || msgCon === 'всем доброго времени суток' || msgCon === 'шалом'){
+        	message.channel.send('Приветствую тебя');
+    	}
+    	if(msgCon === 'бот подкинь монету'){
+		message.channel.send('Подкинул монету...');
+		textprediction = ["Орел", "Решка"];
+		message.channel.send("Выпало: " +textprediction[Math.floor(Math.random() * textprediction.length)]);
+    	}   
+    	if(msgCon === 'бот скажи дату и время'){
+		moment.locale('ru')
+		message.channel.send(`**Точная дата и время:** \`${moment().format('dddd, MMMM DD YYYY, hh:mm:ss')}\``);
+    	}
 }
